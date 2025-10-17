7cd604734540f9e41423b4ed079e7b0293a59e8b
Nishad1005 <164496206+Nishad1005@users.noreply.github.com>
2025-10-18 04:32:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Don’t let CI fail because of lint/TS while we stabilize the codebase.
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
-
 module.exports = nextConfig;
 
